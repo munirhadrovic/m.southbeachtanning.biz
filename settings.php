6bb43946cdb0625a08c3e91d728eb3bdd7c2cfa2
EldinSalkovic <eldinsalkoviic@gmail.com>
2015-05-13 15:20:51 +0200

    First commit

---
+++ settings.php
@@ -0,0 +1,30 @@
+<?php
+	$adresa[0]="10539 South Dixie Highway"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[0]="19155 West Road Woodhave,MI 48183";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[0]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[0]="WOODHAVEN SALON";
+	$telephone[0]=7346751100;// Broj koji se poziva na dugme CALL US
+
+	$adresa[1]="50822 Romeo Plank Macomb MI 48044"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[1]="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[1]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[1]="MACOMB TOWNSHIP SALON";
+	$telephone[1]=5866303135;// Broj koji se poziva na dugme CALL US
+
+	$adresa[2]="48898 Hayes Road Macomb MI 48044"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[2]="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[2]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[2]="MACOMB TOWNSHIP SALON";
+	$telephone[2]=5865329939;// Broj koji se poziva na dugme CALL US
+
+	$adresa[3]="26962 Telegraph Flat Rock,MI 48314"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[3]="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[3]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[3]="FLAT ROCK SALON";
+	$telephone[3]=7347837861;// Broj koji se poziva na dugme CALL US
+	
+	$centratelephone=3052522000;
+	$companynumber=4;
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="flip"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
